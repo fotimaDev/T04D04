cd24500fc3e0e479e50e0173bd3e0192d2c0ab00
Angelyn Chung <angelync@si-b2.kzn.21-school.ru>
2023-09-14 16:45:48 +0300

    3

---
+++ src/quest3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int f(int x) {
+  if (x == 1 || x == 2)
+    return 1;
+  return f(x - 1) + f(x - 2);
+}
+
+int main() {
+  int a;
+  scanf("%d", &a);
+    if (a <= 0) {
+        printf("n/a");
+        return 0;
+    }
+  printf("%d", f(a));
+  return 0;
+}

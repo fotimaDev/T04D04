879c7861b19d52768b1799067fd53d3ddf817f8c
Angelyn Chung <angelync@si-b2.kzn.21-school.ru>
2023-09-14 18:43:07 +0300

    3

---
+++ src/char_decode.c
@@ -0,0 +1,11 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+    int a;
+    while (getchar() != '\n') {
+        a = getchar();
+        printf("%d\n", a);
+    }
+    return 0;
+}
--- src/quest3.c
+++ src/quest3.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
 int f(int x) {
-  if (x == 1 || x == 2)
-    return 1;
-  return f(x - 1) + f(x - 2);
+    if (x == 1 || x == 2) {
+        return 1;
+    }
+
+    return f(x - 1) + f(x - 2);
 }
 
 int main() {
-  int a;
-  scanf("%d", &a);
+    int a;
+    scanf("%d", &a);
     if (a <= 0) {
         printf("n/a");
         return 0;
     }
-  printf("%d", f(a));
-  return 0;
+
+    printf("%d", f(a));
+    return 0;
 }
